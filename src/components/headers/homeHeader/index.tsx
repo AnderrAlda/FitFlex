f1d45c6e18f02782914acb0c0a3f13344ccacddc
Ander Aldabaldetreku <anderaldabaldetreku@gmail.com>
2024-02-23 21:57:05 +0100

    control empy cart in cartpage and number in header

--- src/components/headers/homeHeader/index.tsx
+++ src/components/headers/homeHeader/index.tsx
@@ -6,7 +6,7 @@ import { CartContext } from "../../../context/cartContext";
 type Props = {};
 
 const HomeHeader = (props: Props) => {
-  const { totalAmount } = useContext(CartContext);
+  const { totalAmount, contextData } = useContext(CartContext);
   return (
     <div>
       <div className="flex justify-center relative">
@@ -30,9 +30,14 @@ const HomeHeader = (props: Props) => {
               d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
             ></path>
           </svg>
-          <p className="absolute right-4 top-9 bg-slate-500 rounded-lg w-4 text-center">
-            {totalAmount}
-          </p>
+
+          {contextData.length === 0 ? (
+            <div></div>
+          ) : (
+            <p className="absolute right-4 top-9 bg-slate-500 rounded-lg w-4 text-center">
+              {totalAmount}
+            </p>
+          )}
         </Link>
       </div>
     </div>
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -16,46 +16,55 @@ const CartPage = (props: Props) => {
   return (
     <>
       <DynamicHeader HeaderType={HeaderTypes.Cart} />
+      <div>
+        {contextData.length === 0 ? (
+          <div className="h-60 w-60">
+            <p className="mt-32 ml-32 w-32">The cart is empty</p>
+          </div>
+        ) : (
+          <div>
+            <VerticalScrollLayout height="40rem">
+              {contextData.map((cartItem) => (
+                <ShoppingCartProduct
+                  id={cartItem.id}
+                  key={cartItem.id}
+                  name={cartItem.name}
+                  price={cartItem.price}
+                  image={cartItem.image}
+                  amount={cartItem.amount}
+                  stock={cartItem.stock}
+                />
+              ))}
+            </VerticalScrollLayout>
 
-      <VerticalScrollLayout height="40rem">
-        {contextData.map((cartItem) => (
-          <ShoppingCartProduct
-            id={cartItem.id}
-            key={cartItem.id}
-            name={cartItem.name}
-            price={cartItem.price}
-            image={cartItem.image}
-            amount={cartItem.amount}
-            stock={cartItem.stock}
-          />
-        ))}
-      </VerticalScrollLayout>
-
-      <div className="mt-10">
-        <div className="flex justify-around">
-          <p>Total {totalAmount} items</p>
-          <p className="font-bold">USD {totalPrice}</p>
-        </div>
-        <button className="bg-black text-white rounded-3xl p-3 ml-12 mt-2 w-72   flex justify-around">
-          <p className="font-bold">Proceed to checkout</p>
-          <svg
-            className="h-6"
-            data-slot="icon"
-            data-darkreader-inline-stroke=""
-            fill="none"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m8.25 4.5 7.5 7.5-7.5 7.5"
-            ></path>
-          </svg>
-        </button>
+            <div className="mt-10">
+              <div className="flex justify-around">
+                <p>Total {totalAmount} items</p>
+                <p className="font-bold">USD {totalPrice}</p>
+              </div>
+              <button className="bg-black text-white rounded-3xl p-3 ml-12 mt-2 w-72   flex justify-around">
+                <p className="font-bold">Proceed to checkout</p>
+                <svg
+                  className="h-6"
+                  data-slot="icon"
+                  data-darkreader-inline-stroke=""
+                  fill="none"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
