0ca92e6a4ae4e0fcfdb67b5ef550732d247e75e7
Ander Aldabaldetreku <anderaldabaldetreku@gmail.com>
2024-02-23 08:37:08 +0100

    bottom part of cart

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -19,6 +19,33 @@ const CartPage = (props: Props) => {
         <ShoppingCartProduct />
         <ShoppingCartProduct />
       </VerticalScrollLayout>
+
+      <div className="mt-10">
+        <div className="flex justify-around">
+          <p>Total 2 items</p>
+          <p className="font-bold">USD 750</p>
+        </div>
+        <button className="bg-black text-white rounded-3xl p-3 ml-12 mt-2 w-72   flex justify-around">
+          <p className="font-bold">Proceed to checkout</p>
+          <svg
+            className="h-6"
+            data-slot="icon"
+            data-darkreader-inline-stroke=""
+            fill="none"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m8.25 4.5 7.5 7.5-7.5 7.5"
+            ></path>
+          </svg>
+        </button>
+      </div>
     </>
   );
 };
